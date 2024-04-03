e68d69290115c7b57c93de6b0ed58c7db2b54b2b
j1ngzoue <workdrive0306@gmail.com>
2024-04-03 19:34:10 +0900

    Add app clip target

---
+++ demo/demoClip/demoClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoClipApp.swift
+//  demoClip
+//
+//  Created by j1ngzoue on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct demoClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
