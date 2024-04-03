c009d3a4de0a0a530d05deb669288e848cdcd4c2
j1ngzoue <workdrive0306@gmail.com>
2024-04-03 19:29:25 +0900

    Initial

---
+++ demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by j1ngzoue on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
