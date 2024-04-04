1ab2ee8173f4adadbf9ddfb0b7cc6d94a705f0f3
j1ngzoue <workdrive0306@gmail.com>
2024-04-04 12:35:48 +0900

    コードの変更を記述するための新しいファイルを追加しました。

---
+++ demo2/demo2/demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  demo2App.swift
+//  demo2
+//
+//  Created by j1ngzoue on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demo2/demo2clip/demo2clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo2clipApp.swift
+//  demo2clip
+//
+//  Created by j1ngzoue on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct demo2clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
